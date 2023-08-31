2f91cc4131ae1a2a3c82fc45e1182276e59cde80
Peter morales <peter@mintstatelabs.com>
2023-08-31 08:42:19 -0700

    progress i think

--- Assets/llamatest.cs
+++ Assets/llamatest.cs
@@ -10,7 +10,8 @@ public class llamatest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string modelPath = "<Your model path>";
+        string modelPath = Application.streamingAssetsPath + "/wizardLM-7B.ggmlv3.q4_0.bin";
+        Debug.Log(modelPath);
         string prompt = "what is your purpose?";
         // Initialize a chat session
         var ex = new InteractiveExecutor(new LLamaModel(new ModelParams(modelPath, contextSize: 1024, seed: 1337)));
